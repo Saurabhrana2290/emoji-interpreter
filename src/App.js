8a4941d88fd59e166175f2be7c01321fb1891587
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-07 11:32:02 +0530

    Updated title in index.html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+var emojiDictionary = {
+  "😀": "Grinning face",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "😍": "Smiling Face with Heart-Eyes",
+  "😝": "Squinting Face with Tongue",
+  "✌": "Victory Hand",
+  "✋": "Raised Hand"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function onChangeHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      setMeaning("Emoji is not present in our Database");
+    } else {
+      setMeaning(meaning);
+    }
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>inside outtt</h1>
+      <input onChange={onChangeHandler} />
+      <div>{meaning}</div>
+      <h3>Emojis We Know</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+            onClick={() => emojiClickHandler(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
